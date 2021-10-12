f87daa9758fcec95d792a94a57c498fae99507a3
Kamal Hussain <kamal.hussain@rspb.org.uk>
2021-10-12 16:33:54 +0100

    Todo App with Validation

---
+++ BlazorToDoApp/BlazorToDoApp/Data/Models/TaskModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorToDoApp.Data.Models
+{
+    public class TaskModel
+    {
+        [Required(ErrorMessage = "Task Empty")]
+        [StringLength(50, ErrorMessage ="Too long")]
+        [MinLength(5, ErrorMessage ="Too short")]
+        public string TaskName { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
